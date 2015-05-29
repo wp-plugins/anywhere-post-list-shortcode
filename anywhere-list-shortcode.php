b8cf4b73f89f0963f2bb364901ebc92e5437e5bc
marushu <marushu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-29 20:48:33 +0000

    plugin updated
    git-svn-id: https://plugins.svn.wordpress.org/anywhere-post-list-shortcode/trunk@1170560 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- anywhere-list-shortcode.php
+++ anywhere-list-shortcode.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Anywhere Post List Shortcode
-Version: 0.1
+Version: 0.2
 Plugin URI: http://hibou-web.com
 Description: You can simply put a "[list]" short code, to list the title of the latest article (link with) and the front page of your blog, sidebar, footer. Set category and custom post type, taxonomy also other it is also possible.
 Author: Shuhei Nishimura
@@ -14,7 +14,7 @@ License URI: http://opensource.org/licenses/gpl-2.0.php GPLv2
  * Anywhere Post List Shortcode
  *
  * @package Anywhere Post List Shortcode
- * @version 0.0.1
+ * @version 0.2
  * @author Shuhei Nishimura <shuhei.nishimura@gmail.com>
  * @copyright Copyright (c) 2014 Shuhei Nishimura (Hibou).
  * @license http://opensource.org/licenses/gpl-2.0.php GPLv2
@@ -22,20 +22,23 @@ License URI: http://opensource.org/licenses/gpl-2.0.php GPLv2
  */
 
 function anywhere_list( $atts ) {
-
+	global $post;
 	extract( shortcode_atts( array(
 		'post_type'			=>	'post',
 		'cat_name'			=>	'',
-		'num'						=>	3,
+		'num'						=>	10,
 		'class'					=>	'',
 		'orderby'				=>	'post_date',
 		'order'					=>	'DESC',
 		'length'				=>	'',
 		'end_of_title'	=>	'',
 		'taxonomy'			=>	'',
-		'term'					=>	''
+		'term'					=>	'',
+		'more'					=>	' … more',
+		'post_format'		=>	'standard',
+		'no_filter'			=>	false,
 	), $atts ) );
-	global $post;
+
 	if( $post_type === 'post' ) {
 		$args = array(
 		  'post_type'				=>	$post_type,
@@ -43,7 +46,7 @@ function anywhere_list( $atts ) {
 		  'order'						=>	$order,
 		  'category_name'		=>	$cat_name,
 		  'orderby'					=>	$orderby,
-		  'order'						=>	$order
+		  'order'						=>	$order,
 		);
 	} elseif( ! empty( $taxonomy ) && ! empty( $term ) ) {
 		$args = array(
@@ -55,7 +58,7 @@ function anywhere_list( $atts ) {
 				array(
 					'taxonomy'	=>	$taxonomy,
 					'field'			=>	'slug',
-					'terms'			=>	$term
+					'terms'			=>	$term,
 				)
 			)
 		);
@@ -64,31 +67,58 @@ function anywhere_list( $atts ) {
 		  'post_type'			=>	$post_type,
 		  'posts_per_page'=>	$num,
 		  'order'					=>	$order,
+		  'post_format'		=>	$post_format,
 		);
 	}
-	$relational_posts = get_posts( $args );
-	setup_postdata( $post );
-	$html = '';
-	foreach( $relational_posts as $post ) {
-		$post_link	= get_permalink();
-		$post_title	= get_the_title();
-		
-		if( ! empty( $length ) && mb_strlen( $post_title ) >= $length ) {
-			$post_title = mb_strimwidth( $post_title, 0, $length*2-1, $end_of_title );
+	$list_posts = get_posts( $args );
+
+	$html   = '';
+	$output = '';
+	$class = ( ! empty( $class ) ) ? "class='$class'" : '';
+	if( $list_posts ) {
+		$html .= apply_filters( 'anywhter_before_list_tag', "<ul $class>" );
+		foreach( $list_posts as $post ) {
+			setup_postdata( $post );
+			$post_id    = $post->ID;
+			$post_link	= get_permalink( $post_id );
+			$post_title	= get_the_title( $post_id );
+
+			if( ! empty( $length ) && mb_strlen( $post_title ) >= $length ) {
+				$post_title = wp_trim_words( $post_title, $length, $more );
+			}
+			$post_title = apply_filters( 'the_title', $post_title );
+
+			if( ! empty( $list_posts ) ) {
+				$no_filter ?
+				$html .= apply_filters( 'anywhter_list_content', '<li>'
+							. '<a href="' . esc_url ( $post_link ) . '">'
+							. esc_html( $post_title )
+							. '</a></li>'
+				) :
+				$html .= '<li>'
+							. '<a href="' . esc_url ( $post_link ) . '">'
+							. esc_html( $post_title )
+							. '</a></li>';
+			} else {
+				$no_filter ?
+				$html .= apply_filters( 'anywhter_list_content', '<li>'
+							. '<a href="' . esc_url ( $post_link )
+							. '">'
+							. esc_html( $post_title )
+							. '</a></li>'
+				) :
+				$html .= '<li>'
+							. '<a href="' . esc_url ( $post_link )
+							. '">'
+							. esc_html( $post_title )
+							. '</a></li>';
+			}
 		}
-		$post_title = apply_filters( 'the_title', $post_title );
-		
-		$html .= '<li>' . '<a href="' . $post_link . '">' . $post_title . '</a></li>';
 	}
 	wp_reset_postdata();
-	if( ! empty( $relational_posts ) && ! empty( $class ) ) {
-		$output = "<ul class='$class'>" . $html . "</ul>";
-		return $output;
-	} elseif( ! empty( $relational_posts ) ) {
-		$output =  "<ul>" . $html . "</ul>";
-		return $output;
-	}
-} 
+	$html .= apply_filters( 'anywhter_after_list_tag', '</ul>' );
+	return $html;
+}
 add_shortcode( 'list', 'anywhere_list' );
 
 // add widget area
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors: marushu
 Tags: post, post type
 Requires at least: 3.9.1
-Tested up to: 3.9.1
+Tested up to: 4.2.2
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -15,6 +15,7 @@ List the article :)
 = Fetures =
 WordPress post retrieve and list.
 Purpose to be output by the number specified latest posts mainly is suitable.
+Added filter( anywhter_before_list_tag, anywhter_list_content, anywhter_after_list_tag ).
 
 You can simply put a "[list]" short code, to list the title of the latest article (link with) and the front page of your blog, sidebar, footer. Set category and custom post type, taxonomy also other it is also possible.
 
@@ -31,6 +32,9 @@ Main parameters, [refer to the get_posts of Codex](http://codex.wordpress.org/Te
 - end_of_title: Specify a string to be appended to the end of the article. This must be required for 'length'.  
 - taxonomy: If you want to output by specifying a custom taxonomy.  
 - term: Required for 'taxonomy'.  
+- more: read more link text.
+- post_format: post format
+- no_filter(boolean): If you are creating a filter hook , you are able to set not affected. :)
 
 = Example =
 - [list] most simple.  
@@ -46,6 +50,10 @@ It can also be used in widget :)
 - The plug-in is made effective.  
 
 == Changelog ==
+= 0.2 =
+- add filter
+- no_filter option
+
 = 0.1 =
 - First release :)  
 
